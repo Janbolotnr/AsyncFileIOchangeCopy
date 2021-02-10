d1bf77e6191869b44ed19c329baf5cc15dec840f
Janbolot <janbolotnr@gmail.com>
2021-02-10 10:05:41 +0600

    FileIOchan

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace AsyncFileIOiz
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var from = "life.txt";
+            var to = "october.txt";
+
+            using (FileStream source = File.Open(from, FileMode.Open))
+            {
+                using (FileStream target = File.Open(to, FileMode.Open))
+                {
+                    await source.CopyToAsync(target);
+                }
+            }
+        }
+    }
+}
+
+
+/* var file = File.OpenText("life.txt");
+
+            for (var i = 0; i < 3; i++)
+            {
+                var line = await file.ReadLineAsync();
+                Console.WriteLine($"line {i + 1}: {line}");
+            }*/
